803034f3d0419e4fc7e385dfd81c90f529617ea1
tharunsr <tharunsrinivaassr@gmail.com>
2025-01-18 09:40:42 +0530

    Just pushing

--- src/main/java/com/example/SS/controller/ProductController.java
+++ src/main/java/com/example/SS/controller/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cosmetics")
+@RequestMapping("/cosmetics/product")
 public class ProductController {
 
     @Autowired
